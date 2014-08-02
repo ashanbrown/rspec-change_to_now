81b0d28f1c8e162c8cb023fb4d487cca1cf342e8
Andrew S. Brown <andrew@dontfidget.com>
2014-08-01 22:17:50 -0700

    use fail_matching helper

--- spec/rspec/change_to_now_spec.rb
+++ spec/rspec/change_to_now_spec.rb
@@ -14,18 +14,17 @@ module RSpec
         expect {
           expect {
           }.to change { number }.to_now eq 2
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError,
-                         /did not change/)
+        }.to fail_matching("did not change")
       end
       it "fails without an expectation block" do
         expect {
           expect(1).to change { number }.to_now eq 2
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /was not given a block/)
+        }.to fail_matching("was not given a block")
       end
       it "fails when the argument is not a matcher" do
         expect {
           expect { }.to change { number }.to_now 2
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /to be a matcher but got/)
+        }.to fail_matching("to be a matcher but got")
       end
       it "fails when the final expectation is never met" do
         number = 1
@@ -33,7 +32,7 @@ module RSpec
           expect {
             number += 1
           }.to change { number }.to_now eq 3
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /After the change.*got: 2/m)
+        }.to fail_matching(/After the change.*got: 2/m)
       end
       it "fails when the final expectation is already met" do
         number = 2
@@ -41,7 +40,7 @@ module RSpec
           expect {
             number += 1
           }.to change { number }.to_now eq 2
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /Before the change.*got: 2/m)
+        }.to fail_matching(/Before the change.*got: 2/m)
       end
     end
 
@@ -57,14 +56,14 @@ module RSpec
         expect {
           expect {
           }.to change { number }.not_to eq 1
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /did not change/)
+        }.to fail_matching("did not change")
       end
       it "fails when the final expectation is already met" do
         number = 2
         expect {
           expect {
           }.to change { number }.not_to eq 1
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /Before the change.*got: 2/m)
+        }.to fail_matching(/Before the change.*got: 2/m)
       end
       it "fails when the final expectation is never met" do
         number = 2
@@ -72,7 +71,7 @@ module RSpec
           expect {
             number = 4
           }.to change { number }.not_to satisfy(&:even?)
-        }.to raise_error(RSpec::Expectations::ExpectationNotMetError, /After the change.*not to satisfy/m)
+        }.to fail_matching(/After the change.*not to satisfy/m)
       end
     end
 
